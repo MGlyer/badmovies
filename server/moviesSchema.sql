9b31ab16d6fd15b4a150fd9b9d6237d9ebbe05c5
Hack Reactor Student <student@hackreactor.com>
2018-07-15 14:06:45 -0400

    successfully creates schema

--- server/moviesSchema.sql
+++ server/moviesSchema.sql
@@ -1,1 +1,13 @@
 -- SET UP SCHEMA HERE
+CREATE DATABASE favorites;
+
+USE favorites
+
+CREATE TABLE movies2 (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(150),
+    img VARCHAR(100),
+    votes INT,
+    date VARCHAR(100),
+    PRIMARY KEY (ID)
+);
